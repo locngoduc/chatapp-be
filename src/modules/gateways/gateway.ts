e5b2679464dcb1e4672574765ce865bb9fa0ba6e
trapper268 <tuanhuytruong35@gmail.com>
2025-04-15 16:38:54 +0700

    Add DictionaryService for managing user socket mappings in WebSocket gateway

--- src/modules/gateways/gateway.ts
+++ src/modules/gateways/gateway.ts
@@ -9,32 +9,47 @@ import {
 import { Server, Socket } from 'socket.io';
 import { RedisService } from '../redis/redis.service';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import { DictionaryService } from './dictionary.service';
 
 @WebSocketGateway({ cors: true })
 export class MyGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
+  private readonly logger = new Logger(MyGateway.name);
+
   constructor(
     private readonly redisService: RedisService,
     private readonly configService: ConfigService,
+    private readonly dictionaryService: DictionaryService,
   ) {}
 
   private readonly instanceName = this.configService.get('INSTANCE_NAME');
 
-  async handleConnection(client: Socket) {
-    const user = (client.request as any).user;
+  async handleConnection(socket: Socket) {
+    const user = (socket.request as any).user;
     if (user?.id) {
-      this.redisService.addUserInstance(user.id, this.instanceName);
-      console.log(`User ${user.email} connected on ${this.instanceName}`);
+      await this.redisService.addUserInstance(user.id, this.instanceName);
+      this.logger.log(`User ${user.email} connected on ${this.instanceName}`);
+      this.dictionaryService.addUserSocket(user.id, socket.id);
+      const userSockets = this.dictionaryService.getUserSockets(user.id);
+      this.logger.log(
+        `User ${user.email} has sockets: ${JSON.stringify(userSockets)}`,
+      );
     }
   }
 
-  async handleDisconnect(client: Socket) {
-    const user = (client.request as any).user;
+  async handleDisconnect(socket: Socket) {
+    const user = (socket.request as any).user;
     if (user) {
       await this.redisService.removeUserInstance(user.id, this.instanceName);
       console.log(`User ${user.email} disconnected from ${this.instanceName}`);
+      this.dictionaryService.removeUserSocket(user.id, socket.id);
+      const userSockets = this.dictionaryService.getUserSockets(user.id);
+      this.logger.log(
+        `User ${user.email} has sockets: ${JSON.stringify(userSockets)}`,
+      );
     }
   }
 
